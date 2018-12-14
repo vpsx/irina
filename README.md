49da202c42144b674933d035cc55ca4402170aac
zlc <19900057+vpsx@users.noreply.github.com>
2018-12-13 23:35:02 -0600

    pA6oTa

---
+++ README.md
@@ -0,0 +1,5 @@
+$ node irina.js
+$ curl -X GET http://127.0.0.1:3000/start
+$ curl -X GET http://127.0.0.1:3000/pause
+$ curl -X GET http://127.0.0.1:3000/reset
+$ curl -X GET http://127.0.0.1:3000/stop
---
+++ irina.js
@@ -0,0 +1,88 @@
+// Братан, алё
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+a_timeout_object = null;
+seconds = 0;
+
+//server object is an EventEmitter
+const server = http.createServer();
+server.on('request', (request, response) => {
+  //And this is the request handler
+  //request is instance of IncomingMessage, has method, url, headers properties
+  //const { headers, method, url } = request; meh
+  if (request.method === 'GET' && request.url === '/start') {
+    // let body = []; meh
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+      //body.push(chunk); meh
+    }).on ('end', () => {
+      console.log("GET /start !");
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/plain');
+      //const responseBody = { headers, method, url }; no body bc idc
+      if (a_timeout_object) {
+        response.end('Таймер уже начался. Чтобы обнулить: /reset\n');
+      } else { 
+        if (seconds === 0) {
+          response.write('Таймер начался!\n');
+        } else {
+          response.write('Таймер начался снова!\n');
+        }
+        response.write(`${seconds}\n`);
+        a_timeout_object = setInterval(update_time, 1000);
+        response.end();
+      }
+    });
+  } else if (request.method === 'GET' && request.url === '/reset') {
+    request.on('error', (err) => {
+      console.error(err);
+    }).on('data', (chunk) => {
+    }).on('end', () => {
+      console.log("GET /reset !");
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/plain');
+      response.write(`${seconds}\n`);
+      reset_time(); //FUU
+      response.end();
+    })
+  } else if (request.method === 'GET' && request.url === '/pause') {
+    //Ля мне лень уже написать ответ сервера лол
+    console.log("GET /pause");
+    if (a_timeout_object) {
+      clearInterval(a_timeout_object);
+      a_timeout_object = null;
+    }
+    response.end('Таймер адихает...\n');
+  } else if (request.method === 'GET' && request.url === '/stop') {
+    console.log("GET /stop");
+    if (a_timeout_object) {
+      clearInterval(a_timeout_object);
+      a_timeout_object = null;
+      seconds = 0;
+    }
+    response.end('Таймер умер.\n');
+  } else {
+    response.statusCode = 404;
+    response.end();
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+function update_time() {
+	seconds++;
+	console.log(`seconds = ${seconds}`);
+}
+
+function reset_time() {
+	seconds = 0; //каво
+	console.log(`Reset seconds: ${seconds}`);
+}
